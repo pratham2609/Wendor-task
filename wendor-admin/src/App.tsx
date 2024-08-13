e301d5e41b2ed2119bd234c13d5c89b4ef577340
pratham2609 <pratham.mehta0826@gmail.com>
2024-08-13 22:55:53 +0530

    Frontend and admin panel initialized

---
+++ wendor-admin/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Wendor Admin
+    </>
+  )
+}
+
+export default App
---
+++ wendor-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Wendor Frontend
+    </>
+  )
+}
+
+export default App
